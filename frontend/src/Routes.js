1b0c7875a1272b60ef540c99d9a2daddb9d0fb36
danielomelo <106938202+danielomelo@users.noreply.github.com>
2025-06-30 16:16:58 -0300

    update 3.6

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import Cadastro from './components/Cadastro';
-import Listagem from './components/Listagem';
-import Relatorios from './components/Relatorios';
 
 function RoutesComponent() {
   return (
